15b34b9744dbb6efb097bb71640e513d2a3db0f4
fjribgs <fajribagaskara@upi.edu>
2025-10-22 15:38:32 +0700

    added smooth scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import Lenis from '@studio-freight/lenis'
+
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+window.lenis = lenis;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,28 +6,34 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LokaHub</title>
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     </style>
     @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="">
+    <img src="{{ asset('images/hero-section.png')}}" 
+        alt="Background"
+        class="-z-10 absolute">
     <nav id="navbar"
     class="flex absolute mx-35 my-10">
         <div class="text-4xl font-bold fixed text-white">LokaHub</div>
         <div></div>
     </nav>
     <section id="hero-section" 
-    class="relative flex items-center">
-        <img src="{{ asset('images/hero-section.png')}}" 
-        alt="Background"
-        class="-z-10">
+    class="flex items-center h-300 justify-content-between">
         <div class="absolute mx-35">
-            <h1 class="text-white text-7xl w-90">Temukan <b>UMKM terbaik</b> di sekitarmu!</h1>
+            <h1 class="text-white text-7xl w-90 **mt-6**">Temukan <b>UMKM terbaik</b> di sekitarmu!</h1>
             <button class="font-semibold text-[19px] bg-white rounded-4xl py-[15px] px-[33px] my-[52px]">Jelajahi Sekarang</button>
+        </div>
+        <div>
+            <img src="{{ asset('images/hero-section-2.png')}}" alt=""
+            class="w-[790px] h-[950px] absolute top-[306px] left-[1127px]">
         </div>
     </div>
 
-    <a href="https://maps.app.goo.gl/SqGff3Yg4EUSeL6a7?g_st=aw">link gmaps</a>
-
+    <a href="https://maps.app.goo.gl/SqGff3Yg4EUSeL6a7?g_st=aw"
+    class="hidden">link gmaps</a>
+    
 </body>
 </html>
