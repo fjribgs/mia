a80fafe7f5e195863ff9dde2caef1b36278152de
fjribgs <fajribagaskara@upi.edu>
2025-10-22 22:33:11 +0700

    style: changed hero section background

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav id="navbar"
+    class="flex fixed py-10 px-35 justify-between items-center w-screen">
+    <div>
+        <h1 class="text-4xl font-bold text-white">LokaHub</h1>
+    </div> 
+    <div class="">
+        <button class="border-green-200 border-1 rounded-3xl px-[28px] py-[13px] text-green-200 font-bold">Login</button>
+        <button class="border-green-500 border-1 rounded-3xl px-[28px] py-[13px] text-white font-bold bg-green-500">Register</button>
+    </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,34 +8,24 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     </style>
-    @vite('resources/css/app.css')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <img src="{{ asset('images/hero-section.png')}}" 
-        alt="Background"
-        class="-z-10 absolute">
-    <nav id="navbar"
-    class="flex fixed py-10 px-35 justify-between items-center w-screen">
-        <div>
-            <h1 class="text-4xl font-bold text-white">LokaHub</h1>
-        </div> 
-        <div>
-            <button class="border-green-200 border-1 rounded-3xl px-[28px] py-[13px] text-green-200 font-bold">Login</button>
-            <button class="border-green-500 border-1 rounded-3xl px-[28px] py-[13px] text-white font-bold bg-green-500">Register</button>
-        </div>
-    </nav>
+    <img src="{{ asset('images/hero-section.svg')}}" 
+            alt="Background"
+            class="-z-10 absolute w-full">
+
+    <x-navbar></x-navbar>
+
     <section id="hero-section" 
-    class="flex items-center h-300 justify-content-between">
-        <div class="absolute mx-35">
+    class="flex items-center h-270 justify-between w-full px-35 rounded-b-2xl">
+        <div>
             <h1 class="text-white text-[74px] w-140">Temukan <b>UMKM terbaik</b> di sekitarmu!</h1>
             <button class="font-semibold text-[19px] bg-white rounded-4xl py-[15px] px-[33px] my-[52px]">Jelajahi Sekarang</button>
         </div>
-        <div>
-            <img src="{{ asset('images/hero-section-2.png')}}" alt=""
-            class="w-[790px] h-[950px] absolute top-[306px] left-[1127px]">
-        </div>
-    </div>
+    </section>
+
+    <x-footer></x-footer>
 
     <a href="https://maps.app.goo.gl/SqGff3Yg4EUSeL6a7?g_st=aw"
     class="hidden">link gmaps</a>
