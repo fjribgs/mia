f1f3ba59eb7c3ee88a61b80626decfdb21631760
fjribgs <fajribagaskara@upi.edu>
2025-10-25 20:46:14 +0700

    feat: updated navbar and footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,21 +1,47 @@
-<div>
-    <div class="bg-[var(--primary-700)] h-[490px] px-[60px]">
+<div class="">
+
+    <div class="bg-[var(--primary-700)] h-[490px] md:h-[450px] px-[60px] md:px-36">
+
         <div class="py-[60px]">
+
             <img src="{{ asset('images/logo.svg') }}" alt=""
             class="w-50">
-            <p class="text-gray-300 my-10 font-light text-[12px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet porttitor interdum. Donec eu ante et libero feugiat ultrices.</p>
+
+            <p class="text-gray-300 my-10 font-light text-[12px] md:text-[18px] md:max-w-[560px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet porttitor interdum. Donec eu ante et libero feugiat ultrices.</p>
+
             <div class="flex gap-[15px]">
                 <img src="{{ asset('images/instagram.svg') }}" alt="Instagram">
                 <img src="{{ asset('images/linkedin.svg') }}" alt="LinkedIn">
             </div>
+
         </div>
-        <div class="h-[1px] bg-gray-400 "></div>
-        <div class="relative">
-            <p class="text-gray-300 font-light mt-10 text-[10px]">Copyright 2025 LokaHub. All Rights Reserved</p>
-            <div class="flex gap-2 mt-2">
-                <p class="text-gray-300 font-light text-[10px]">Privacy Policy</p>
-                <p class="text-gray-300 font-light text-[10px]">Terms & Conditions</p>
+
+        <div class="h-[1px] bg-gray-400"></div>
+
+        <div class="flex justify-between text-[10px] md:text-[13px]">
+
+            <p class="text-gray-300 font-light mt-10 md:mt-4">
+                Copyright 2025 LokaHub. All Rights Reserved
+            </p>
+
+            <div class="flex md:hidden gap-2 mt-2">
+
+                <p class="text-gray-300 font-light">Privacy Policy</p>
+
+                <p class="text-gray-300 font-light">Terms & Conditions</p>
+
             </div>
+
+            <div class="flex gap-2 mt-4">
+
+                <p class="text-gray-300 font-light">Privacy Policy</p>
+
+                <p class="text-gray-300 font-light">Terms & Conditions</p>
+
+            </div>
+
         </div>
+
     </div>
+
 </div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,25 +1,31 @@
 <nav id="navbar"
     class="flex fixed py-10 px-[36px] md:px-35 justify-between items-center w-screen">
+
     <div>
-        <img src="{{ asset('images/logo.svg') }}" alt=""
-            class="xl:w-43 h-auto w-26 md:w-40">
+        <a href="/">
+            <img src="{{ asset('images/logo.svg') }}" alt=""
+                class="xl:w-43 h-auto w-26 md:w-40">
+        </a>
     </div> 
+
     <div class="xl:flex gap-[35px] font-[Montserrat] hidden">
         <a href="#"
-            class="text-white">Beranda</a>
+            class="text-white hover:font-bold transition-all ease-in">Beranda</a>
         <a href="#" 
-            class="text-white">Fitur</a>
+            class="text-white hover:font-bold transition-all ease-in">Fitur</a>
         <a href="#"
-            class="text-white">Kolaborasi</a>
+            class="text-white hover:font-bold transition-all ease-in">Kolaborasi</a>
         <a href="#"
-            class="text-white">Event</a>
+            class="text-white hover:font-bold transition-all ease-in">Event</a>
         <a href="#"
-            class="text-white">Tentang Kami</a>
+            class="text-white hover:font-bold transition-all ease-in">Tentang Kami</a>
     </div>
+
     <div class="hidden xl:block">
         <button class="border-white border-1 rounded-3xl px-[25px] py-[10px] text-white font-semibold">Login</button>
         <button class="border-white border-1 rounded-3xl px-[25px] py-[10px] text-black font-medium bg-white">Register</button>
     </div>
+
     <div class="xl:hidden">
         <img src="{{ asset('images/sidebar.svg')}}" alt="Sidebar"
             class="md:scale-150">
